ff0fbacc88abf64482e8d3dbab44ba0f2c6c7aab
rtzarius <rtzarius@gmail.com>
2014-05-19 19:16:31 +1000

    Async testing working OK

--- mocha-tests/test-a.js
+++ mocha-tests/test-a.js
@@ -6,22 +6,25 @@ var assert = require('assert');
 var lap = require('../less-autoprefixed');
 
 describe('basic-test', function () {
+
     it('should produce the expected unminified output', function (done) {
-        assert.equal(
-                '/* comment here */\n' +
-                '.testclass {\n' +
-                '  display: -webkit-box;\n' +
-                '  display: -webkit-flex;\n' +
-                '  display: -ms-flexbox;\n' +
-                '  display: flex;\n' +
-                '}',
-            lap.basicChain(
-                '/* comment here */\n.testclass{display:flex;}\n',
-                function(err, data) {
-                    if (err) throw err;
-                    done();
-                }
-            )
+        lap.basicChain(
+            '/* comment here */\n.testclass{display:flex;}\n',
+            function (err, data) {
+                if (err) throw err;
+
+                assert.equal(
+                    data,
+                        '/* comment here */\n' +
+                        '.testclass {\n' +
+                        '  display: -webkit-box;\n' +
+                        '  display: -webkit-flex;\n' +
+                        '  display: -ms-flexbox;\n' +
+                        '  display: flex;\n' +
+                        '}\n'
+                );
+                done();
+            }
         );
     })
 });
